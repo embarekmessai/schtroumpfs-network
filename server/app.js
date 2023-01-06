f7ce00038910dfff7253f6482dd6f3686260fa78
embarekmessai <m_mebarek@hotmail.com>
2023-01-06 06:45:58 +0100

    Add: Create a new mongoose connection Refactor: Fix mongoose strictQuery warning

--- server/app.js
+++ server/app.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false);
+
+main().catch(err => console.log(err));
+
+async function main(){
+    await mongoose.connect('mongodb://localhost:27017/db_schtroumpfs')
+            .then(() => console.log("DB Connection Successfull!"))
+            .catch((err) => {
+                console.log(err);
+            });
+
+}
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('serve is running successfully');
