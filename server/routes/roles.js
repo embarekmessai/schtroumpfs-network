31327e89fd8f0da5e5e95638ec5f9db0626e2ba0
embarekmessai <m_mebarek@hotmail.com>
2023-01-06 10:41:07 +0100

    Add: Create add new role method

--- server/routes/roles.js
+++ server/routes/roles.js
@@ -10,3 +10,20 @@ router.get('/', async(req, res) => {
         res.status(500).json(err);
     }
 })
+
+// Add new role
+router.post('/roles', async(req, res)=>{
+    try {
+        const newRole = new Role({
+            name: req.body.name
+        });
+
+        await newRole.save();
+
+        res.status(201).json('Le role a été créé avec succès!');
+        
+    } catch (error) {
+        const status = error.status || 500;
+        res.status(status).json(error);
+    }
+})
