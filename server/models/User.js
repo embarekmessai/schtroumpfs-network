dfbfd68138c2e128f3fd2f35a1d27a8bf8743962
embarekmessai <m_mebarek@hotmail.com>
2023-01-06 22:29:12 +0100

    Add: Create token field for user model Refactor: Change role type as Object

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,8 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     fullname: { type: String, required: true, unique: true },
     password: { type: String, required: true, select: false },
-    role: mongoose.Mixed
+    role: {type: Object},
+    token: {type: String}
 }, { timestamps: true })
 
 module.exports = mongoose.model("User", userSchema);
