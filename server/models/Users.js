6d8b81e8b074ae210f68d90516b10eb33af637af
embarekmessai <m_mebarek@hotmail.com>
2023-01-06 09:10:47 +0100

    Refactor: Add role to user schema

--- server/models/Users.js
+++ server/models/Users.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     fullname: { type: String, required: true, unique: true },
     password: { type: String, required: true, select: false },
+    role: mongoose.Mixed
 }, { timestamps: true })
 
 module.exports = mongoose.model("User", userSchema);
