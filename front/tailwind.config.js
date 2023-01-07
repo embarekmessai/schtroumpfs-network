8a9029b9d1b57ef6306ab2eabc58df4c3d34840d
embarekmessai <m_mebarek@hotmail.com>
2023-01-07 20:02:27 +0100

    Add: Add tailwind principals colors

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    colors:{
+      ...colors,
+      appGreen : '#12B10F',
+      appBlue : '#26C7FA'
+    },
     extend: {},
   },
   plugins: [],
