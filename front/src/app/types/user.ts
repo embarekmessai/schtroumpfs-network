eb84c53a70e89f9ad621cdcadb7112968fa244b4
embarekmessai <m_mebarek@hotmail.com>
2023-01-09 22:49:09 +0100

    Add: Create a user profile type

--- front/src/app/types/user.ts
+++ front/src/app/types/user.ts
@@ -3,4 +3,16 @@ export interface User {
   fullname : string | null,
   password : string | null,
   confirme_password ?: string | null
-}
+};
+
+// User role interface
+
+export type UserProfile = {
+  roles : {name ?: string | null}[],
+  user: {
+    username ?: string | null,
+    fullname ?: string | null,
+    role ?: string | null,
+    password ?: string | null,
+  },
+};
