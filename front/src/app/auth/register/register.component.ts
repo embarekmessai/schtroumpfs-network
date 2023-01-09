2949c7467aadc120a3cd051912d2fcedcc19cbe1
embarekmessai <m_mebarek@hotmail.com>
2023-01-09 01:58:03 +0100

    Add: Implement registration process Add: Create registration form & submit method Add: Integrate reactive params to template form Add: Alert for errors

--- front/src/app/auth/register/register.component.ts
+++ front/src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder
+  ){ };
+
+  registerForm = this.formBuilder.group({
+    username: ['', Validators.required],
+    fullname: [''],
+    password: [''],
+    password_conformation: [''],
+  });
+  isSuccessful = false;
+  isRegistredFailed = false;
+  errorMessage = '';
+
+  onSubmit = () => {
+
+    this.authService.register(this.registerForm.value).subscribe(
+      data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isRegistredFailed = false;
+
+      },
+
+      err => {
+        this.errorMessage = err.error;
+        this.isRegistredFailed = true;
+      }
+    )
+  }
+
+
 
 }
