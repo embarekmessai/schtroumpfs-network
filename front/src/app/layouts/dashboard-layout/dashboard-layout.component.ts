c2f2d96b88601a1389cb59da90d3d430e1bf3008
embarekmessai <m_mebarek@hotmail.com>
2023-01-10 07:42:46 +0100

    Add: Set a dynamic avatar to user based on role

--- front/src/app/layouts/dashboard-layout/dashboard-layout.component.ts
+++ front/src/app/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,24 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { faPowerOff } from '@fortawesome/free-solid-svg-icons';
 import { AuthService } from '../../auth/auth.service';
 import { Router } from '@angular/router';
+import { ProfileService } from '../../profile/profile.service';
 
 @Component({
   selector: 'app-dashboard-layout',
   templateUrl: './dashboard-layout.component.html',
   styleUrls: ['./dashboard-layout.component.css']
 })
-export class DashboardLayoutComponent {
+export class DashboardLayoutComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private profileService: ProfileService
   ) { }
 
+  ngOnInit(): void {
+    console.log('profile init');
+
+
+    // Get profile datas
+    this.profileService.getProfile().subscribe(
+      res => {
+        // Get profile information
+        this.avatar = res?.user.role?.avatar;
+      },
+      err => {
+        window.sessionStorage.removeItem(this.authService.user_key);
+        this.router.navigate(['/auth/login'])
+        console.log(err);
+
+      }
+    );
+  }
   // Icon
   faPowerOff = faPowerOff
 
-  profileMenu = false
+  profileMenu = false;
+  avatar: any = null;
 
   openProfileMenu: any = () => {
     this.profileMenu = !this.profileMenu
