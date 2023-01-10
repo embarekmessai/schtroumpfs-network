d81be37bb4ffc121e9aaaae6520c816df566fe7c
embarekmessai <m_mebarek@hotmail.com>
2023-01-10 22:54:24 +0100

    Refactor: Assign auth api to auth service to use it globaly

--- front/src/app/auth/auth.service.ts
+++ front/src/app/auth/auth.service.ts
@@ -2,7 +2,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-const auth_api = 'http://127.0.0.1:5000/api/v1';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -16,16 +15,18 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
+  auth_api = 'http://127.0.0.1:5000/api/v1';
+
   user_key = 'auth-user';
 
   // Registration method
   register(data : any): Observable<any> {
-    return this.http.post(`${auth_api}/register`, data ,httpOptions);
+    return this.http.post(`${this.auth_api}/register`, data ,httpOptions);
   }
 
   // Login method
   login(data : any): Observable<any> {
-    return this.http.post(`${auth_api}/login`, data ,httpOptions);
+    return this.http.post(`${this.auth_api}/login`, data ,httpOptions);
   }
 
   // Login method
@@ -33,7 +34,7 @@ export class AuthService {
     const userSession: any = window.sessionStorage.getItem(this.user_key);
     const user = JSON.parse(userSession);
 
-    return this.http.post(`${auth_api}/logout`, null, { headers: new HttpHeaders({'Authorization' : `Bearer ${user.accessToken}`})})
+    return this.http.post(`${this.auth_api}/logout`, null, { headers: new HttpHeaders({'Authorization' : `Bearer ${user.accessToken}`})})
   }
 
   // Check if user is authenticated
